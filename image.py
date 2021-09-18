73af6d08215bea10f8dcaccbb6a0b0ac35a0ac2a
vrushned <80047748+vrushned@users.noreply.github.com>
2021-09-18 19:29:51 +0530

    Create image.py

---
+++ image.py
@@ -0,0 +1,17 @@
+import sys
+import os
+from PIL import Image
+
+input_folder=sys.argv[1]
+output_folder=sys.argv[2]
+
+if not os.path.exists(output_folder):
+	os.makedirs(output_folder)
+
+for filename in os.listdir(input_folder):
+	img =Image.open(f'{input_folder}{filename}')
+	clean_name=os.path.splitext(filename)[0]
+	#print(clean_name)
+	img.save(f'{output_folder}{clean_name}.png','png')
+
+print("Done!")
